b590969ea85db53ed2b00f187a98759375f0dfd3
adebayo-789 <desmondobaleye9@gmail.com>
2025-04-11 18:46:31 +0100

    I've added a flyout menu page and made a recipe page

--- assignment-2425/MainPage.xaml.cs
+++ assignment-2425/MainPage.xaml.cs
@@ -9,16 +9,11 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void  OnMyRecipiesClicked(object sender, EventArgs e)
         {
-            count++;
+            await Shell.Current.GoToAsync(nameof(RecipeListPage));
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            SemanticScreenReader.Announce(RecipesBtn.Text);
         }
     }
 
