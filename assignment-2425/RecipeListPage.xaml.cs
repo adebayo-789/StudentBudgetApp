b62ed504a5773f650cafb31fbeee95ed5c6ee17c
adebayo-789 <desmondobaleye9@gmail.com>
2025-04-12 12:24:46 +0100

    Added a add recipe form and a view recipe section

--- assignment-2425/RecipeListPage.xaml.cs
+++ assignment-2425/RecipeListPage.xaml.cs
@@ -1,20 +1,52 @@
+using System.Collections.ObjectModel;
+
 namespace assignment_2425;
 
 public partial class RecipeListPage : ContentPage
 {
-	public RecipeListPage()
-	{
-		InitializeComponent();
-		
-			Content = new VerticalStackLayout
-			{
-				Children = {
-					new Label { HorizontalOptions = LayoutOptions.Center, 
-								VerticalOptions = LayoutOptions.Center,
-								Text = "Welcome to .NET MAUI!"
-					}
-				}
-			};
-		
-	}
+    public class Recipe
+    {
+        public string? Name { get; set; }
+        public string? Ingredients { get; set; }
+        public string? Steps { get; set; }
+    }
+
+
+        private ObservableCollection<Recipe> recipeList = new ObservableCollection<Recipe>();
+
+        public RecipeListPage()
+        {
+            InitializeComponent();
+            recipesCollectionView.ItemsSource = recipeList;
+        }
+
+        private async void OnAddRecipeClicked(object sender, EventArgs e)
+        {
+            string? name= recipeNameEntry.Text?.Trim();
+            string?ingredients = ingredientsEditor.Text?.Trim();
+            string?steps = stepsEditor.Text?.Trim();
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(ingredients) || string.IsNullOrEmpty(steps))
+            {
+                await DisplayAlert("Missing Info", "Please fill in all fields.", "OK");
+                return;
+            }
+
+            var newRecipe = new Recipe
+            {
+                Name = name,
+                Ingredients = ingredients,
+                Steps = steps
+            };
+
+            recipeList.Add(newRecipe);
+
+
+            recipeNameEntry.Text = "";
+            ingredientsEditor.Text = "";
+            stepsEditor.Text = "";
+
+            await DisplayAlert("Success", $"Recipe '{name}' added!", "OK");
+        }
+    
 }
---
+++ assignment-2425/SettingsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2425
+{
+    internal class SettingsPage
+    {
+    }
+}
