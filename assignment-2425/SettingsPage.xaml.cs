ba670bf09563cfa888b2e077671870ce2451cc5b
adebayo-789 <desmondobaleye9@gmail.com>
2025-04-13 13:29:05 +0100

    Atempting to add Drak Mode, Font Size change and Font change feature to settings page

--- assignment-2425/SettingsPage.xaml.cs
+++ assignment-2425/SettingsPage.xaml.cs
@@ -3,18 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
+
 
 namespace assignment_2425;
 
-public partial class SettingPage : ContentPage
 
+public partial class SettingsPage : ContentPage
 {
+    public SettingsPage()
+    {
+        InitializeComponent();
 
+        // Load saved settings
+        fontSizePicker.SelectedIndex = Preferences.Default.Get("FontSizeIndex", 1); // Default to Medium
+        darkModeSwitch.IsToggled = Preferences.Default.Get("DarkMode", false);
 
+        ApplyFontSize(fontSizePicker.SelectedIndex);
+        ApplyTheme(darkModeSwitch.IsToggled);
+    }
 
+    void OnFontSizeChanged(object sender, EventArgs e)
+    {
+        int selectedIndex = fontSizePicker.SelectedIndex;
+        Preferences.Default.Set("FontSizeIndex", selectedIndex);
+        ApplyFontSize(selectedIndex);
+    }
 
+    void ApplyFontSize(int index)
+    {
+        double fontSize = index switch
+        {
+            0 => 12, // Small
+            1 => 16, // Medium
+            2 => 20, // Large
+            _ => 16
+        };
+
+        Application.Current.Resources["AppFontSize"] = fontSize;
+    }
+
+    void OnDarkModeToggled(object sender, ToggledEventArgs e)
+    {
+        bool isDark = e.Value;
+        Preferences.Default.Set("DarkMode", isDark);
+        ApplyTheme(isDark);
+    }
+
+    void ApplyTheme(bool isDark)
+    {
+        App.Current.UserAppTheme = isDark ? AppTheme.Dark : AppTheme.Light;
+    }
 
 
 
 }
+
+
+
+
+
+
+
+
+
 
