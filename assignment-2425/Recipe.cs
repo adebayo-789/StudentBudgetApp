4b3234e15862193e794a47ab520674dcbdaf1609
adebayo-789 <desmondobaleye9@gmail.com>
2025-04-13 17:24:38 +0100

    finalised changes to the dark mode

---
+++ assignment-2425/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2425
+{
+    internal class Recipe
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Ingredients { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Instructions { get; set; } = string.Empty;
+    }
+}
