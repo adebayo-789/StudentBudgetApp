3e4e23b0bf66ae7016378b29919ac0b9d3839c63
adebayo-789 <desmondobaleye9@gmail.com>
2025-05-03 00:58:39 +0100

    idk

---
+++ assignment-2425/Pages/AddExpensePage.xaml.cs
@@ -0,0 +1,19 @@
+using System.Text.Json;
+using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using StudentBudgetApp.ViewModels;
+
+namespace StudentBudgetApp.Pages
+{ 
+public partial class AddExpensePage : ContentPage
+{
+    private readonly BudgetViewModel _viewModel;
+
+    public AddExpensePage(BudgetViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+}
+}
